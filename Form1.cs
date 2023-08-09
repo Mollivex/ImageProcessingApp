f888e63f5d052887967b5d8aaed9b8d1f791fd1d
Alex <licoms1997@gmail.com>
2023-08-09 07:36:12 +0300

    Add GetPixels method (Color, Point)

--- Form1.cs
+++ Form1.cs
@@ -27,13 +27,49 @@ namespace ImageProcessingApp
         {
             if(openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                pictureBox1.Image = Bitmap.FromFile(openFileDialog1.FileName);
+                // clear the last one image
+                pictureBox1.Image = null;
+                _bitmaps.Clear();
+
+                // declare a new variable where our new image will save
+                var bitmap = new Bitmap(openFileDialog1.FileName);
             }
+        }
+
+        /// <summary>
+        /// Processing opened image
+        /// </summary>
+        private void RunProcessing(Bitmap bitmap)
+        {
+
+        }
+
+        /// <summary>
+        /// Get information about each pixel of image
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        private List<Pixel> GetPixels(Bitmap bitmap)
+        {
+            var pixels = new List<Pixel>(bitmap.Height * bitmap.Width);
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    pixels.Add(new Pixel()
+                    {
+                        Color = bitmap.GetPixel(x, y),
+                        Point = new Point() { X = x, Y = y }
+                    });
+                }
+            }
+            return pixels;
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            // exception handlings if list of elements _bitmap is null or doesn't has any elements
+            // exception handlings if the list of elements _bitmap is null or exist, but doesn't has any elements
             if (_bitmaps == null || _bitmaps.Count == 0)
                 return;
 
